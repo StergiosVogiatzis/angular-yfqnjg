cb591fa0d2e4415bec9807104916c959feae6ae2
StergiosVogiatzis <tehvog@gmail.com>
2022-06-22 11:22:33 +0300

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -22,4 +22,9 @@ export class CartComponent implements OnInit {
       address: '',
     });
   }
+  onSubmit(customerData) {
+    console.warn('Your order has been submitted', customerData);
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
 }
